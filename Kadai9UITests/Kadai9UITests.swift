2d8ff2bce9d7fcd92ddcccdec78ba603fbece8e2
daikiumehara <daikiumehara3@gmail.com>
2021-08-13 03:15:40 +0900

    UIテストを記述しました

--- Kadai9UITests/Kadai9UITests.swift
+++ Kadai9UITests/Kadai9UITests.swift
@@ -9,13 +9,11 @@ import XCTest
 
 class Kadai9UITests: XCTestCase {
 
+    let app = XCUIApplication()
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app.launch()
     }
 
     override func tearDownWithError() throws {
@@ -23,12 +21,48 @@ class Kadai9UITests: XCTestCase {
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+        // 初めにlabelが未選択になっているか確認
+        var name = "未選択"
+        searchString(name)
+        // 次のページに行って何もせずに戻った時、labelが未選択のままになっているか確認
+        tapNextButton()
+        tapCancelButton()
+        searchString(name)
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // 次のページに行って東京都のボタンを押して戻った時、labelが東京都になっているか確認
+        name = "東京都"
+        tapNextButton()
+        tapNameButton(name)
+        tapCancelButton()
+        searchString(name)
+
+        // 次のページに行って何もせずに戻った時、labelが東京都のままになっているか確認
+        tapNextButton()
+        tapCancelButton()
+        searchString(name)
+
+        // 次のページに行って埼玉県のボタンを押して戻った時、labelが埼玉県になっているか確認
+        name = "埼玉県"
+        tapNextButton()
+        tapNameButton(name)
+        tapCancelButton()
+        searchString(name)
+    }
+
+    func tapNextButton() {
+        app.buttons["入力"].tap()
+    }
+
+    func tapCancelButton() {
+        app.buttons["キャンセル"].tap()
+    }
+
+    func tapNameButton(_ name: String) {
+        app.buttons["\(name)"].tap()
+    }
+
+    func searchString(_ string: String) {
+        XCTAssert(app.staticTexts["\(string)"].isHittable)
     }
 
     func testLaunchPerformance() throws {
